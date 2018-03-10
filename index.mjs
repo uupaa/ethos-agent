15dca324cd416b071cc73d4fee4e27dba6d30e17
uupaa <uupaa.js@gmail.com>
2018-03-10 14:48:26 +0900

    fix

--- index.mjs
+++ index.mjs
@@ -7,12 +7,13 @@ export class EthOSStatus {
     this._state = {
       stopped: false,
       detail: {
+        url:          "",     // control panel url
+        problem:      false,  // has problem
         miner_enable: false,  // `allow` is true, `disallow` is false
         miner_proxy:  false,  // miner proxy has error -> false
         cpu_temp:     0,      // cpu temp number
-        gpu_crashed:  [],     // crashed gpu ids
         gpu_count:    0,      // gpu count
-        problem:      false,  // has problem
+        gpu_crashed:  [],     // crashed gpu ids
       },
     };
   }
@@ -21,13 +22,13 @@ export class EthOSStatus {
   }
   update() {
     const META = {
+      url:          { path: "/var/run/ethos/url.file",          fn: b => b.trim() },
+      problem:      { path: "/var/run/ethos/status.file",       fn: b => /problem/.test(b) },
       miner_enable: { path: "/opt/ethos/etc/allow.file",        fn: b => parseInt(b) === 1 },
       miner_proxy:  { path: "/var/run/ethos/proxy_error.file",  fn: b => b.trim() === "working" },
       cpu_temp:     { path: "/var/run/ethos/cputemp.file",      fn: b => parseInt(b) },
-      gpu_crashed:  { path: "/var/run/ethos/crashed_gpus.file", fn: b => b.trim.split(/\s+/).map(n => parseInt(n)) },
       gpu_count:    { path: "/var/run/ethos/gpucount.file",     fn: b => parseInt(b) },
-      problem:      { path: "/var/run/ethos/status.file",       fn: b => /problem/.test(b) },
-      url:          { path: "/var/run/ethos/url.file",          fn: b => b.trim() },
+      gpu_crashed:  { path: "/var/run/ethos/crashed_gpus.file", fn: b => b.trim().length ? b.trim().split(/\s+/).map(n => parseInt(n)) : [] },
     };
     for (const key in META) {
       const { path, fn } = META[key];
