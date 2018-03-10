ba2b4e99169926f3248c4dbfc02e0f3fa67f94ca
uupaa <uupaa.js@gmail.com>
2018-03-10 15:54:03 +0900

    add log and verbose mode

--- index.mjs
+++ index.mjs
@@ -4,10 +4,15 @@ import EthOSWatch from "./lib/EthOSWatch";
 
 const watch = new EthOSWatch({
   notifyURL: "", // Slack WebHookURL: "https://hooks.slack.com/services/T00000000/B00000000/xxxxxxxxxxxxxxxxxxxxxxxx";
+  verbose: 0,
 });
 
 setTimeout(() => {
+
+  console.log("EthOSHelper start watch");
   watch.start();
 
 }, 1000 * 60 * 5); // 5 min after
+
+console.log("EthOSHelper running");
 
