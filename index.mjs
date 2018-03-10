240646d24585608d832716cd8e8bcebe78e27a86
uupaa <uupaa.js@gmail.com>
2018-03-10 22:20:03 +0900

    support rc.local exec

--- index.mjs
+++ index.mjs
@@ -1,7 +1,12 @@
 #!/usr/bin/node --experimental-modules
 
+import path from "path";
 import EthOSHelperConfig from "./lib/EthOSHelperConfig";
 import EthOSWatch from "./lib/EthOSWatch";
+
+// change working dir to /home/ethos/tools/EthOSHelper.js
+// because execute from rc.local
+process.chdir( path.parse( process.argv[1] ).dir );
 
 const conf = new EthOSHelperConfig().json;
 const watch = new EthOSWatch(conf);
